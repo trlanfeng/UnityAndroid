c6a10425f98dfb68ca3684cab9ef8b5cb8306d44
孤月蓝风 <trlanfeng@foxmail.com>
2017-12-11 16:06:53 +0800

    第二部分及第三部分的代码

---
+++ 3/androidlib/src/main/java/com/test/androidlib/LibActivity.java
@@ -0,0 +1,34 @@
+package com.test.androidlib;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class LibActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lib);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra("com.test.androidapp.message");
+        TextView lib_textview = (TextView) findViewById(R.id.lib_textview);
+        lib_textview.setText(message);
+
+        Button button = (Button) findViewById(R.id.lib_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                String message = "456";
+                intent.putExtra("com.test.androidlib.message",message);
+                setResult(1,intent);
+                finish();
+            }
+        });
+    }
+}
---
+++ 3/app/src/main/java/com/test/androidapp/MainActivity.java
@@ -0,0 +1,40 @@
+package com.test.androidapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.test.testunity.UnityPlayerActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.main_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, UnityPlayerActivity.class);
+                startActivity(intent);
+
+        //                Intent intent = new Intent(MainActivity.this,com.test.androidlib.LibActivity.class);
+        //                String message = "123";
+        //                intent.putExtra("com.test.androidapp.message",message);
+        //                startActivityForResult(intent,1);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        TextView main_textview = (TextView) findViewById(R.id.main_textview);
+        main_textview.setText(data.getStringExtra("com.test.androidlib.message"));
+    }
+}
